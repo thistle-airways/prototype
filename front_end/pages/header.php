edecd36fc91d3117b0b94b4335c82d6a3ffe08b9
Michael Shannon <michaelrshannon@gmail.com>
2011-01-25 00:51:06 +0000

    Work on developing basic framework for frontend

---
+++ front_end/pages/header.php
@@ -0,0 +1,8 @@
+<div class="menu_left">
+  <ul id="menu_left_affiche">
+    <li class="menu_princ_hover" style=";"><span id="lien_62"> <a id="lien_62_value" href="NewPage">Main</a></span></li>
+    <li class="menu_princ" style=";"> <span id="lien_63"> <a id="lien_63_value" href="NewPagel">Log in</a> </span> </li>
+    <li class="menu_princ" style=";"> <span id="lien_64"> <a id="lien_64_value" href="NewPage">Your Account</a> </span> </li>
+    <li class="menu_princ" style=";"> <span id="lien_65"> <a id="lien_65_value" href="NewPage">Flights</a> </span> </li>
+  </ul>
+</div>
