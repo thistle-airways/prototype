ef01be26acae200c0c8c4da2ce7b6eebdd60f451
Craig John George Matear <cm226@jove.(none)>
2011-01-25 22:49:16 +0000

    flight select and flightInfo pages added

---
+++ front_end/pages/selectedFlight.php
@@ -0,0 +1,24 @@
+
+<h2>flight: 203</h2>
+
+<table id="flightData" border=1>
+<tr><th>flight number:</th><td>203</td></tr>
+<tr><th>flight departure:</th><td>Edi</td></tr>
+<tr><th>flight destination:</th><td>Lut</td></tr>
+<tr><th>flight departure date:</th><td>2011-02-16</td></tr>
+<tr><th>flight departure time:</th><td>08:00</td></tr>
+<tr><th>flight arrival time:</th><td>09:00</td></tr>
+<tr><th>seat avalability:</th><td>
+		<table>
+		<tr><td>econemy: </td><td>20</td><tr>
+		<tr><td>business: </td><td>20</td><tr>
+		<tr><td>group: </td><td>20</td><tr>
+		
+		</table>
+</td></tr>
+
+</table>
+
+<br/><br/>
+<button type="button" onClick="window.location='bookingInfo.html'">book flight 203</button>
+
